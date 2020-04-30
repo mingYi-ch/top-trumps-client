db2f0fb45dc98f5a8f22750fb85e5eba9f7ce147
Rohit Kaushik <kaushikrohit325@gmail.com>
2020-04-30 03:18:06 +0200

    update login page to materials-ui

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,157 +1,99 @@
 import React, {Component} from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { withRouter } from "react-router-dom";
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import FormLabel from '@material-ui/core/FormLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const BaseContainer = styled.div`
-  margin-left: auto;
-  padding-left: 15px;
-  margin-right: auto;
-  padding-right: 15px;
-  max-width: 1160px;
-`;
+const genres = [
+  'all',
+  'adult',
+  'adventure'
+];
 
-const FormContainer = styled.div`
-  margin-top: 3em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 300px;
-  justify-content: center;
-`;
+const MyBox = styled(Box)({
+  background: 'linear-gradient(90deg, #20B2AA 30%, #e1ddbc 80%)',
+  border: 2,
+  borderRadius: 20,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  spacing: '20px',
+  padding: '10px 10px 10px 10px'
+});
 
-const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 50%;
-  height: 550px;
-  font-size: 16px;
-  font-weight: 300;
-  padding-left: 37px;
-  padding-right: 37px;
-  border-radius: 15px;
-  background: linear-gradient(#20B2AA, #e1ddbc);
-  transition: opacity 0.5s ease, transform 0.5s ease;
-`;
-
-const FormSmall= styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const InputField = styled.input`
-  &::placeholder {
-    color: grey;
-  }
-  height: 35px;
-  padding-left: 30px;
-  padding-top: 6px;
-  margin-left: -4px;
-  border: none;
-  border-radius: 20px;
-  margin-bottom: 30px;
-  background: rgba(255, 255, 255, 0.2);
-  color: white;
-`;
-
-const InputFieldSmall = styled.input`
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.2);
-  }
-  height: 35px;
-  width: 50px
-  padding-left: 30px;
-  padding-top: 6px;
-  margin-left: 15px;
-  margin-right: 15px;
-  border: none;
-  border-radius: 20px;
-  margin-bottom: 30px;
-  background: rgba(255, 255, 255, 0.2);
-  color: white;
-`;
-
-const Label = styled.label`
-  color: white;
-  margin-bottom: 8px;
-  text-transform: uppercase;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 50px;
-`;
-
-const Button= styled.button`
-  width: 30%;
-  margin-top: 10px;
-
-`;
+const MyContainer = styled(Container)({
+  spacing: '40px',
+  padding: '10px 10px 10px 10px'
+});
 
 
+const MyButton = styled(Button)({
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  padding: '0 30px',
+});
 
 class Login extends Component {
 
+    constructor(props) {
+      super(props);
+      this.state = {'genre': 'all'};
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+      this.setState((state) => {
+        return {genre: e.target.value}
+      });
+      //localStorage.setItem(key, e.target.value);
+    };
+
     render(){
         return (
-            <BaseContainer>
-                <FormContainer>
-                    <Form>
-                        <Label>Player Name</Label>
-                        <InputField
-                            // placeholder={this.state.username}
-                            onChange={e => {
-                                localStorage.setItem("username",e.target.value);
-                        }}/>
-
-                        <Label>Year Range of Movies</Label>
-                        <FormSmall>
-                            <Label style = {{textTransform:"lowercase"}}>From</Label>
-                            <InputFieldSmall
-                            // placeholder={this.state.username}
-                            onChange={e => {
-                                localStorage.setItem("username",e.target.value);
-                            }}/>
-
-                            <Label style = {{textTransform:"lowercase"}}>to</Label>
-                            <InputFieldSmall
-                            // placeholder={this.state.username}
-                            onChange={e => {
-                                localStorage.setItem("username",e.target.value);
-                            }}/>
-                        </FormSmall>
-
-
-                        <Label>genre of Movies</Label>
-                        <InputField list = "genre" autoComplete = "on"
-                            placeholder="all"
-                            onChange={e => {
-                                localStorage.setItem("username",e.target.value);
-                        }}/>
-                        <datalist id="genre">
-                          <option value="all"/>
-                          <option value="adult"/>
-                          <option value="adventure"/>
-                        </datalist>
-
-                        <ButtonContainer>
-                          <Button 
-                            onClick = {() => {this.props.history.push("/rules")}}
-                          >
-                            View Game Rule
-                          </Button>
-                          <Button
-                            onClick = {() => {this.props.history.push("/game")}}
-                          >
-                            Let's Play!
-                          </Button>
-                        </ButtonContainer>
-                    </Form>
-                </FormContainer>
-            </BaseContainer>
+          <Grid container direction="column" alignItems="center" justify="center">
+            <MyBox>
+            <form noValidate autoComplete="on">
+              <MyContainer>
+                <FormLabel component="legend">Enter your nick:</FormLabel>
+                <TextField id="name" label="Player's Name" variant="outlined" />
+              </MyContainer>
+              <MyContainer>
+                <FormLabel component="legend">Year Range of Movies</FormLabel>
+                <TextField id="from" label="starting year" variant="outlined" />
+                <TextField id="to" label="ending year" variant="outlined" />
+              </MyContainer>
+              <MyContainer>
+                <TextField
+                  id="genre"
+                  select
+                  label="movie genre"
+                  value={this.state.genre}
+                  onChange={this.handleChange}
+                  helperText='Please select the movie genre'
+                >
+                  {genres.map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </MyContainer>
+            </form>
+            <MyContainer>
+              <MyButton onClick = {() => {this.props.history.push("/rules")}}>
+                Game Rules
+              </MyButton>
+              <MyButton onClick = {() => {this.props.history.push("/game")}}>
+                Lets Play!
+              </MyButton>
+            </MyContainer>
+          </MyBox>
+          </Grid>
         )
     }
 
